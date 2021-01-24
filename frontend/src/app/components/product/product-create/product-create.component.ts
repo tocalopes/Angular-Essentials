cab715d112544ff285a4e2999099c0b9030ce5d9
gabriel <gabriellopes112009@gmail.com>
2021-01-23 23:21:12 -0300

    criação do service, e implementação do snack bar

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-create',
@@ -7,15 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  niceAttribute = "qualquer";
-
-  constructor() { }
+  constructor(private productService:ProductService, private router:Router) { }
 
   ngOnInit(): void {
+    
   }
 
-  doSomething(): void {
-    console.log("fazendo algo!")
+  createProduct(): void {
+    this.productService.showMessage("Produto criado");
   }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
 
 }
