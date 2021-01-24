d9e8f3ee40b984531705820d5b35ad8c67c80e1c
gabriel <gabriellopes112009@gmail.com>
2021-01-24 11:53:02 -0300

    read service e criação da tabela de renderização de produtos

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,24 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+
+  //injeção de dependência é feita na declaração no construtor
+
+  constructor(private service: ProductService) { }
+  products : Product[];
+
+  ngOnInit(): void {
+    this.service.read().subscribe(products => {
+      this.products = products;
+      console.log(products);
+    })
+  }
+
+}
