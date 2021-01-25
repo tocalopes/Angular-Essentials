0418a501afb833c678fb50163c985fa128bdc2ad
gabriel <gabriellopes112009@gmail.com>
2021-01-25 19:14:51 -0300

    componente de exclusão de produto

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-delete',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDeleteComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+
+  constructor(private service: ProductService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.readById(id).subscribe(
+      (product) => {
+        this.product = product
+      }
+    );
   }
 
+  deleteProduct(): void {
+    this.service.delete(this.product.id.toString()).subscribe(
+      product => {
+        this.service.showMessage(`Produto ${product.name} excluido`);
+        this.router.navigate(['/products'])
+      }
+    );
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
